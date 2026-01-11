fa3e8be14748fd6b075ec350c3371dba03c2452e
vivdglo <a.meles@student.utwente.nl>
2026-01-11 22:47:30 +0100

    message

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Aidana Meles ProfDev Project 2024
-const richHUD = document.getElementById("richHUD");
-let hudShown = false;
+// // Aidana Meles ProfDev Project 2024
+// const richHUD = document.getElementById("richHUD");
+// let hudShown = false;
 
-function toggleMenu() {
-const menu = document. querySelector (".menu-links");
-const icon = document.querySelector(".hamburger-icon");
-menu.classList.toggle( "open");
-icon.classList.toggle("open");
-}
-console.log(results.detections[0].locationData.relativeBoundingBox);
+// function toggleMenu() {
+// const menu = document. querySelector (".menu-links");
+// const icon = document.querySelector(".hamburger-icon");
+// menu.classList.toggle( "open");
+// icon.classList.toggle("open");
+// }
+// console.log(results.detections[0].locationData.relativeBoundingBox);
 
